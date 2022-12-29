ed9f23755687144c0212901a29a471a4f8493fc5
Alexandra ogubuike <ogubuikealex@gmail.com>
2022-12-29 23:01:37 +0100

    Added logger store and formatter

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,6 +1,14 @@
 const pino = require("pino");
-
 module.exports = pino({
     enabled: true,
-    level: "info",
-});
+    level: process.env.PINO_LOG_LEVEL || "info",
+    formatters: { //Change the level to its string value
+        level: label => {
+            return {
+                level: label
+            }
+        },
+        
+    },   
+}, 
+pino.destination(`${__dirname}/logs.csv`));
--- src/utils/server.ts
+++ src/utils/server.ts
@@ -54,11 +54,11 @@ export class SetupServer {
         );
         this.app.use(async (req, res, next) => {
             if (req.headers["x-access-token"]) {                
-                const accessToken = req.headers["x-access-token"];
+                const accessToken: string = req.headers["x-access-token"];
                 const { walletAddress, exp } = jwt.verify(accessToken, process.env.JWT_SECRET);
                 
                 // Check if token has expired
-                if (exp < Date.now().valueOf() / 1000) {                    
+                if (exp < Date.now().valueOf() / 1000) {                                       
                     return res.status(401).json({
                         error: "JWT token has expired, please login to obtain a new one",
                     });
